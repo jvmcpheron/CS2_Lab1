f182c80bb732c39c564c8abb0f5d8e7112d7ad73
jvmcpheron <janemcpheron@gmail.com>
2023-09-07 14:23:50 -0500

    added given code

--- main.cpp
+++ main.cpp
@@ -13,23 +13,31 @@ using namespace std;
 
 
 // base class
-class SElement{
-    public:
+struct SElement{
+    virtual void showValueAndType() = 0;
 };
 
-class EmptyValue{
+struct Empty : SElement{
+    virtual void showValueAndType(){cout<< "Empty";}
+};
+
+struct Integer : SElement{
+    int value;
+    virtual void showValueAndType() {cout << "Int(" <<to_string(value) << ")";}
+    Integer(int val){value = val;}
+};
+
+struct Float : SElement{
+    float value;
+    virtual void showValueAndType() {cout << "Float(" <<to_string(value) << ")";}
+    Float(float val){value = val;}
 
 };
 
-class IntegerValue{
-
-};
-
-class FloatValue{
-
-};
-
-class StringValue{
+struct String : SElement{
+    string value;
+    virtual void showValueAndType() {cout << "String(" <<value << ")";}
+    String(string val){value = val;}
 
 };
 
