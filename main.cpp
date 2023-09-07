a9de5257137bc28df484f9feb2f78a9c581ad8f4
jvmcpheron <janemcpheron@gmail.com>
2023-09-07 14:14:36 -0500

    basic setup complete

---
+++ main.cpp
@@ -0,0 +1,39 @@
+/* 
+CS 2 Lab 1
+Jane McPheron
+*/
+
+ 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+
+// base class
+class SElement{
+    public:
+};
+
+class EmptyValue{
+
+};
+
+class IntegerValue{
+
+};
+
+class FloatValue{
+
+};
+
+class StringValue{
+
+};
+
+int main(){
+
+    return 0;
+}
